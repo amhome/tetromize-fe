4fa17be2e5a3dba70994c4967e86373de855fa13
amhome <amirhosseinmehrvarzi@gmail.com>
2025-02-13 17:45:02 -0500

    recipe comp. implementation

--- src/app/components/recipes/recipes.component.ts
+++ src/app/components/recipes/recipes.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IRecipe } from '../../interfaces/recipe.interface';
+import { DataProviderService } from '../../services/data-provider.service';
 
 @Component({
   selector: 'app-recipes',
   templateUrl: './recipes.component.html',
   styleUrl: './recipes.component.scss'
 })
-export class RecipesComponent {
+export class RecipesComponent implements OnInit{
 
+  records: IRecipe[] = [];
+
+  constructor(private _dataService: DataProviderService) { }
+  
+  ngOnInit(): void {
+    this.getAll();
+  }
+
+  getAll() {
+    this._dataService.getRecipes().subscribe(resp => this.records = resp);
+  }
 }
